8427339fe5226ed617adbeaeb4f13dd6c5380b23
SebasL07 <sebasgnv0207@gmail.com>
2026-01-21 17:09:05 -0500

    first commit

---
+++ src/main/java/com/dispocol/dispofast/DispofastApplication.java
@@ -0,0 +1,13 @@
+package com.dispocol.dispofast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispofastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispofastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dispocol/dispofast/DispofastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dispocol.dispofast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispofastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
