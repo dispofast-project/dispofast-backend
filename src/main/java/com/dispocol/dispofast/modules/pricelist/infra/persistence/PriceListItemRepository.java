429325a5ae5fd83563e9d6af4c9ea43f2d30b848
SebasL07 <sebasgnv0207@gmail.com>
2026-03-16 13:42:54 -0500

    chore: spotless applied

--- src/main/java/com/dispocol/dispofast/modules/pricelist/infra/persistence/PriceListItemRepository.java
+++ src/main/java/com/dispocol/dispofast/modules/pricelist/infra/persistence/PriceListItemRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PriceListItemRepository extends JpaRepository<PriceListItem, UUID> {
 
-  Optional<PriceListItem> findByPriceList_IdAndProduct_Reference(UUID priceListId, String reference);
+  Optional<PriceListItem> findByPriceList_IdAndProduct_Reference(
+      UUID priceListId, String reference);
 
   List<PriceListItem> findByPriceList_Id(UUID priceListId);
 
