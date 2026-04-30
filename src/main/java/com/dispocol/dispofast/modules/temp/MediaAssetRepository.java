9e1f3ed58e784741887605d9aed021ab72c3a203
Silem Nabib <silemnabib@gmail.com>
2026-04-29 19:45:54 -0500

    feat: implement legal document management with upload, download, and delete functionalities

---
+++ src/main/java/com/dispocol/dispofast/modules/temp/MediaAssetRepository.java
@@ -0,0 +1,6 @@
+package com.dispocol.dispofast.modules.temp;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaAssetRepository extends JpaRepository<MediaAsset, UUID> {}
